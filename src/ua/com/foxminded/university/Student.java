ee14b40f7311b876d42f81cafe43162f2285d04b
Egor Anchutin <zbit16@gmail.com>
2021-01-06 01:02:09 +0300

    Add Lombook to project

--- src/ua/com/foxminded/university/Student.java
+++ src/ua/com/foxminded/university/Student.java
@@ -1,80 +1,26 @@
 package ua.com.foxminded.university;
 
-import java.util.List;
-import java.util.Objects;
+import lombok.Data;
 
-public class Student {
+import java.util.List;
+
+public @Data class Student {
 
     private final String firstName;
     private final String lasString;
-    private final int age;
+    private int age;
     private Group group;
     private List<Course> courses;
 
     public Student(String firstName,
                    String lasString,
-                   int age, Group group,
+                   int age,
+                   Group group,
                    List<Course> courses) {
         this.firstName = firstName;
         this.lasString = lasString;
         this.age = age;
         this.group = group;
         this.courses = courses;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLasString() {
-        return lasString;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public Group getGroup() {
-        return group;
-    }
-
-    public void setGroup(Group group) {
-        this.group = group;
-    }
-
-    public List<Course> getCourses() {
-        return courses;
-    }
-
-    public void setCourses(List<Course> courses) {
-        this.courses = courses;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Student student = (Student) o;
-        return age == student.age &&
-                firstName.equals(student.firstName) &&
-                lasString.equals(student.lasString) &&
-                Objects.equals(group, student.group) &&
-                Objects.equals(courses, student.courses);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(firstName, lasString, age, group, courses);
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "firstName='" + firstName + '\'' +
-                ", lasString='" + lasString + '\'' +
-                ", age=" + age +
-                ", group=" + group +
-                ", courses=" + courses +
-                '}';
     }
 }
--- src/ua/com/foxminded/university/Teacher.java
+++ src/ua/com/foxminded/university/Teacher.java
@@ -1,64 +1,23 @@
 package ua.com.foxminded.university;
 
-import java.util.List;
-import java.util.Objects;
+import lombok.Data;
 
-public class Teacher {
+import java.util.List;
+
+public @Data class Teacher {
+
     private final String firstName;
     private final String lastName;
-    private final int age;
+    private int age;
     private List<Course> courses;
 
-    public Teacher(String firstName, String lastName, int age, List<Course> courses) {
+    public Teacher(String firstName,
+                   String lastName,
+                   int age,
+                   List<Course> courses) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
         this.courses = courses;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public List<Course> getCourses() {
-        return courses;
-    }
-
-    public void setCourses(List<Course> courses) {
-        this.courses = courses;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Teacher teacher = (Teacher) o;
-        return age == teacher.age &&
-                Objects.equals(firstName, teacher.firstName) &&
-                Objects.equals(lastName, teacher.lastName) &&
-                Objects.equals(courses, teacher.courses);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(firstName, lastName, age);
-    }
-
-    @Override
-    public String toString() {
-        return "Teacher{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", age=" + age +
-                ", courses=" + courses +
-                '}';
     }
 }
