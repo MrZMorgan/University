6485387778fc15b8e162ea3a667041d954f53f49
Egor Anchutin <zbit16@gmail.com>
2021-01-10 19:31:21 +0300

    Add JdbcTemplate

--- src/ua/com/foxminded/university/Main.java
+++ src/ua/com/foxminded/university/Main.java
@@ -3,15 +3,16 @@ package ua.com.foxminded.university;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
+import ua.com.foxminded.university.dao.GroupsDao;
 
 public class Main {
 
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         JdbcTemplate jdbcTemplate = (JdbcTemplate) context.getBean("jdbcTemplate");
 
-        jdbcTemplate.update("INSERT INTO groups (name) VALUES (?)", "Группа");
+        GroupsDao groupsDao = new GroupsDao(jdbcTemplate);
+        groupsDao.create();
     }
 
 }
