7acf8e84cfd360b688a43065ee0a6f19d94d4756
Egor Anchutin <zbit16@gmail.com>
2021-01-13 00:25:06 +0300

    Move queries to constants

--- src/ua/com/foxminded/university/dao/GroupsJdbcDao.java
+++ src/ua/com/foxminded/university/dao/GroupsJdbcDao.java
@@ -1,14 +1,22 @@
 package ua.com.foxminded.university.dao;
 
 import org.springframework.jdbc.core.JdbcTemplate;
-import ua.com.foxminded.university.dao.mappers.CourseMapper;
 import ua.com.foxminded.university.dao.mappers.GroupMapper;
-import ua.com.foxminded.university.models.Course;
 import ua.com.foxminded.university.models.Group;
 
 import java.util.List;
 
 public class GroupsJdbcDao {
+
+    private static final String INSERT_INTO = "INSERT INTO groups (name) VALUES (?)";
+    private static final String READ = "SELECT FROM groups WHERE id = ?";
+    private static final String READ_ALL = "SELECT * FROM groups";
+    private static final String READ_GROUPS_RELATED_TO_COURSES = "SELECT name FROM groups " +
+                                                                 "JOIN groups_courses ON groups.id = groups_courses.id " +
+                                                                 "WHERE course_id = ?";
+    private final static String UPDATE = "UPDATE groups SET name = ? WHERE id = ?";
+    private final static String DELETE = "DELETE FROM groups WHERE id = ?";
+
     private final JdbcTemplate jdbcTemplate;
 
     public GroupsJdbcDao(JdbcTemplate jdbcTemplate) {
@@ -16,35 +24,32 @@ public class GroupsJdbcDao {
     }
 
     public void create(String groupName) {
-        jdbcTemplate.update("INSERT INTO groups (name) VALUES (?)", groupName);
+        jdbcTemplate.update(INSERT_INTO, groupName);
     }
 
     public Group read(int courseId) {
-        return jdbcTemplate.query("SELECT * FROM courses WHERE id = ?", new Object[]{courseId}, new GroupMapper())
+        return jdbcTemplate.query(READ, new Object[]{courseId}, new GroupMapper())
                 .stream()
                 .findAny()
                 .orElse(null);
     }
 
     public List<Group> read() {
-        return jdbcTemplate.query("SELECT * FROM students", new GroupMapper());
+        return jdbcTemplate.query(READ_ALL, new GroupMapper());
     }
 
     public List<Group> readGroupsRelatedToCourse(int courseId) {
-        return jdbcTemplate.query(
-                "SELECT name FROM groups " +
-                    "JOIN groups_courses ON groups.id = groups_courses.id " +
-                    "WHERE course_id = ?",
+        return jdbcTemplate.query(READ_GROUPS_RELATED_TO_COURSES,
                 new Object[]{courseId}, new GroupMapper());
     }
 
     public void update(int id, Group groupForQuery) {
-        jdbcTemplate.update("UPDATE group SET name=? WHERE id=?",
+        jdbcTemplate.update(UPDATE,
                 groupForQuery.getGroupName(),
                 id);
     }
 
     public void delete(int groupId) {
-        jdbcTemplate.update("DELETE FROM groups WHERE id=?", groupId);
+        jdbcTemplate.update(DELETE, groupId);
     }
 }
--- src/ua/com/foxminded/university/dao/StudentJdbcDao.java
+++ src/ua/com/foxminded/university/dao/StudentJdbcDao.java
@@ -10,6 +10,16 @@ import java.util.List;
 
 public class StudentJdbcDao {
 
+    private static final String CREATE = "INSERT INTO students (first_name, last_name, age, group_id) VALUES (?, ?, ?, ?)";
+    private static final String READ = "SELECT * FROM courses WHERE id = ?";
+    private static final String READ_STUDENTS_RELATED_TO_GROUP = "SELECT * FROM students WHERE group_id = ?";
+    private static final String READ_STUDENTS_RELATED_TO_COURSE = "SELECT first_name, last_name, age, group_id FROM students " +
+                                                                  "JOIN students_courses; ON students.id = students_courses.id " +
+                                                                  "WHERE course_id = ?";
+    private static final String READ_ALL = "SELECT * FROM students";
+    private static final String UPDATE = "UPDATE students SET fists_name=?, last_name = ?, age = ?, group_id = ? WHERE id=?";
+    private static final String DELETE = "DELETE FROM students WHERE id=?";
+
     private final JdbcTemplate jdbcTemplate;
 
     public StudentJdbcDao(JdbcTemplate jdbcTemplate) {
@@ -17,37 +27,31 @@ public class StudentJdbcDao {
     }
 
     public void create(String firstName, String lastName, int age, int groupId) {
-        jdbcTemplate.update("INSERT INTO students (firts_name, last_name, age, group_id) " +
-                "VALUES (?, ?, ?, ?)", firstName, lastName, age, groupId);
+        jdbcTemplate.update(CREATE, firstName, lastName, age, groupId);
     }
 
     public Student read(int studentId) {
-        return jdbcTemplate.query("SELECT * FROM courses WHERE id = ?",
-                new Object[]{studentId}, new StudentMapper())
+        return jdbcTemplate.query(READ, new Object[]{studentId}, new StudentMapper())
                 .stream()
                 .findAny()
                 .orElse(null);
     }
 
     public List<Student> readStudentsRelatedToGroup(int groupId) {
-        return jdbcTemplate.query("SELECT * FROM students WHERE group_id = ?",
+        return jdbcTemplate.query(READ_STUDENTS_RELATED_TO_GROUP,
                 new Object[]{groupId}, new StudentMapper());
     }
 
     public List<Student> readStudentsRelatedToCourse(int courseId) {
-        return jdbcTemplate.query(
-                "SELECT first_name, last_name, age, group_id FROM students " +
-                        "JOIN students_courses; ON students.id = students_courses.id " +
-                        "WHERE course_id = ?",
-                new Object[]{courseId}, new GroupMapper());
+        return jdbcTemplate.query(READ_STUDENTS_RELATED_TO_COURSE, new Object[]{courseId}, new StudentMapper());
     }
 
     public List<Student> read() {
-        return jdbcTemplate.query("SELECT * FROM students", new StudentMapper());
+        return jdbcTemplate.query(READ_ALL, new StudentMapper());
     }
 
     public void update(int id, Student studentForQuery) {
-        jdbcTemplate.update("UPDATE students SET firts_name=?, last_name = ?, age = ?, group_id = ? WHERE id=?",
+        jdbcTemplate.update(UPDATE,
                 studentForQuery.getFirstName(),
                 studentForQuery.getLastName(),
                 studentForQuery.getAge(),
@@ -56,6 +60,6 @@ public class StudentJdbcDao {
     }
 
     public void delete(int studentId) {
-        jdbcTemplate.update("DELETE FROM students WHERE id=?", studentId);
+        jdbcTemplate.update(DELETE, studentId);
     }
 }
