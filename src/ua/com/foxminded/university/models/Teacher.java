f5cc689b9361fd06aa9ce2c5d62bfae795a46b16
Egor Anchutin <zbit16@gmail.com>
2021-01-17 21:15:56 +0300

    Add Lombok

--- src/ua/com/foxminded/university/models/Teacher.java
+++ src/ua/com/foxminded/university/models/Teacher.java
@@ -1,8 +1,10 @@
 package ua.com.foxminded.university.models;
 
-import java.util.List;
-import java.util.Objects;
+import lombok.Data;
 
+import java.util.List;
+
+@Data
 public class Teacher {
 
     private final int id;
@@ -21,61 +23,5 @@ public class Teacher {
         this.lastName = lastName;
         this.age = age;
         this.courses = courses;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public List<Course> getCourses() {
-        return courses;
-    }
-
-    public void setCourses(List<Course> courses) {
-        this.courses = courses;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Teacher teacher = (Teacher) o;
-        return id == teacher.id &&
-                age == teacher.age &&
-                firstName.equals(teacher.firstName) &&
-                lastName.equals(teacher.lastName) &&
-                Objects.equals(courses, teacher.courses);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, firstName, lastName, age, courses);
-    }
-
-    @Override
-    public String toString() {
-        return "Teacher{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", age=" + age +
-                ", courses=" + courses +
-                '}';
     }
 }
