cf1a6e26e5b3d810f9510c4fd4ed9fec1f99c540
Egor Anchutin <zbit16@gmail.com>
2021-02-04 00:56:32 +0300

    Add able to delete row from courses table.

--- src/main/java/ua/com/foxminded/university/controllers/CoursesController.java
+++ src/main/java/ua/com/foxminded/university/controllers/CoursesController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ua.com.foxminded.university.models.Course;
 import ua.com.foxminded.university.models.Group;
 import ua.com.foxminded.university.services.CoursesService;
@@ -29,5 +31,11 @@ public class CoursesController {
         model.addAttribute("allCourses", courses);
 
         return "courses/all-courses";
+    }
+
+    @GetMapping("/{id}")
+    public String deleteCourse(@PathVariable int id) {
+        coursesService.deleteCourseById(id);
+        return "redirect:/courses";
     }
 }
