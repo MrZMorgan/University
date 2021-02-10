484eb03cf31c9496e65e13978a75c6eb13b0c128
Egor Anchutin <zbit16@gmail.com>
2021-02-10 18:33:40 +0300

    Edit test fo StudentsJdbcDao

--- test/ua/com/foxminded/university/dao/StudentsJdbcDaoTest.java
+++ test/ua/com/foxminded/university/dao/StudentsJdbcDaoTest.java
@@ -10,6 +10,7 @@ import ua.com.foxminded.university.entities.Course;
 import ua.com.foxminded.university.entities.Group;
 import ua.com.foxminded.university.entities.Student;
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,6 +23,8 @@ class StudentsJdbcDaoTest {
     private StudentsJdbcDao studentsJdbcDao;
     @Autowired
     private GroupsJdbcDao groupsJdbcDao;
+    @Autowired
+    private CoursesJdbcDao coursesJdbcDao;
 
     @Test
     @Transactional
@@ -43,21 +46,14 @@ class StudentsJdbcDaoTest {
     @Test
     @Transactional
     void shouldReadStudent() {
-        List<Course> coursesForTest = new LinkedList<>();
-
-        String student1FirstName = "Egor";
-        String student1LastName = "Anchutin";
-        int student1Age = 20;
-        int student1GroupId = 1;
-        Group student1group = groupsJdbcDao.read(student1GroupId);
-
-        Student expectedStudent = new Student(student1FirstName, student1LastName, student1Age, student1group);
+        Student expectedStudent = firstExpectedStudent();
         Student actualStudent = studentsJdbcDao.read(1);
 
         assertEquals(expectedStudent, actualStudent);
     }
 
     @Test
+    @Transactional
     void shouldReadAllStudents() {
         List<Student> actualStudentsList = studentsJdbcDao.read();
         List<Student> expectedStudentsList = createStudentsListForTest();
@@ -69,7 +65,6 @@ class StudentsJdbcDaoTest {
     @Transactional
     void shouldUpdateStudent() {
         Group groupForTest = groupsJdbcDao.read(1);
-        List<Course> coursesForTest = new LinkedList<>();
 
         String firstNameForTest = "Egor";
         String lastNameForTest = "Anchutin";
@@ -97,40 +92,55 @@ class StudentsJdbcDaoTest {
         assertEquals(expectedTableSize, actualTableSize);
     }
 
-    private List<Student> createStudentsListForTest() {
-        List<Student> students = new LinkedList<>();
-
-        List<Course> coursesForTest = new LinkedList<>();
-
+    private Student firstExpectedStudent() {
         String student1FirstName = "Egor";
         String student1LastName = "Anchutin";
         int student1Age = 20;
         int student1GroupId = 1;
         Group student1group = groupsJdbcDao.read(student1GroupId);
+        List<Course> student1courses = coursesJdbcDao.readCoursesByStudentId(1);
+
         Student student1 = new Student(student1FirstName, student1LastName, student1Age, student1group);
+        student1.setId(1);
+        student1.setCourses(student1courses);
+
+        return student1;
+    }
+
+    private List<Student> createStudentsListForTest() {
+        List<Student> students = new ArrayList<>();
 
         String student2FirstName = "Guinea";
         String student2LastName = "Pig";
         int student2Age = 21;
         int student2GroupId = 1;
         Group student2group = groupsJdbcDao.read(student2GroupId);
+        List<Course> student2courses = coursesJdbcDao.readCoursesByStudentId(2);
         Student student2 = new Student(student2FirstName, student2LastName, student2Age, student2group);
+        student2.setId(2);
+        student2.setCourses(student2courses);
 
         String student3FirstName = "Olga";
         String student3LastName = "Petrova";
         int student3Age = 23;
         int student3GroupId = 2;
         Group student3group = groupsJdbcDao.read(student3GroupId);
+        List<Course> student3courses = coursesJdbcDao.readCoursesByStudentId(3);
         Student student3 = new Student(student3FirstName, student3LastName, student3Age, student3group);
+        student3.setId(3);
+        student3.setCourses(student3courses);
 
         String student4FirstName = "Yaroslava";
         String student4LastName = "Igorevna";
         int student4Age = 24;
         int student4GroupId = 2;
         Group student4group = groupsJdbcDao.read(student4GroupId);
+        List<Course> student4courses = coursesJdbcDao.readCoursesByStudentId(4);
         Student student4 = new Student(student4FirstName, student4LastName, student4Age, student4group);
+        student4.setId(4);
+        student4.setCourses(student4courses);
 
-        students.add(student1);
+        students.add(firstExpectedStudent());
         students.add(student2);
         students.add(student3);
         students.add(student4);
