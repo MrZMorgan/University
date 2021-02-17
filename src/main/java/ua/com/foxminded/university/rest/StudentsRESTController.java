bcfcee4de66c8236d1a3564f44c3483d46881b0e
Egor Anchutin <zbit16@gmail.com>
2021-02-17 19:31:48 +0300

    Add REST controllers

---
+++ src/main/java/ua/com/foxminded/university/rest/StudentsRESTController.java
@@ -0,0 +1,50 @@
+package ua.com.foxminded.university.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ua.com.foxminded.university.entities.Student;
+import ua.com.foxminded.university.services.interfaces.StudentsService;
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+public class StudentsRESTController {
+
+    private static final String DELETE_MESSAGE = "Student with id %d was deleted";
+    private StudentsService studentsService;
+
+    @Autowired
+    public StudentsRESTController(StudentsService studentsService) {
+        this.studentsService = studentsService;
+    }
+
+    @GetMapping()
+    private List<Student> showAllStudents() {
+        List<Student> students = studentsService.readTable();
+        return students;
+    }
+
+    @GetMapping("/{id}")
+    public Student getStudent(@PathVariable int id){
+        Student student = studentsService.readOneRecordFromTable(id);
+        return student;
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteStudent(@PathVariable int id) {
+        studentsService.deleteStudentById(id);
+        return String.format(DELETE_MESSAGE, id);
+    }
+
+    @PostMapping()
+    public Student createStudent(@RequestBody Student student) {
+        studentsService.createStudent(student);
+        return student;
+    }
+
+    @PutMapping()
+    public Student updateStudent(@RequestBody Student student) {
+        studentsService.createStudent(student);
+        return student;
+    }
+}
---
+++ src/main/java/ua/com/foxminded/university/rest/TeachersRESTController.java
@@ -0,0 +1,54 @@
+package ua.com.foxminded.university.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ua.com.foxminded.university.entities.Student;
+import ua.com.foxminded.university.entities.Teacher;
+import ua.com.foxminded.university.services.interfaces.StudentsService;
+import ua.com.foxminded.university.services.interfaces.TeacherService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/teachers")
+public class TeachersRESTController {
+
+
+    private static final String DELETE_MESSAGE = "Teacher with id %d was deleted";
+    private TeacherService teacherService;
+
+    @Autowired
+    public TeachersRESTController(TeacherService teacherService) {
+        this.teacherService = teacherService;
+    }
+
+    @GetMapping()
+    private List<Teacher> showAllTeachers() {
+        List<Teacher> teachers = teacherService.readTable();
+        return teachers;
+    }
+
+    @GetMapping("/{id}")
+    public Teacher getTeacher(@PathVariable int id){
+        Teacher teacher = teacherService.readOneRecordFromTable(id);
+        return teacher;
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteTeacher(@PathVariable int id) {
+        teacherService.deleteTeacherById(id);
+        return String.format(DELETE_MESSAGE, id);
+    }
+
+    @PostMapping()
+    public Teacher createTeacher(@RequestBody Teacher teacher) {
+        teacherService.createTeacher(teacher);
+        return teacher;
+    }
+
+    @PutMapping()
+    public Teacher updateTeacher(@RequestBody Teacher teacher) {
+        teacherService.createTeacher(teacher);
+        return teacher;
+    }
+}
